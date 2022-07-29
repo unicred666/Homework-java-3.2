2793bde9ac08dc83e41d3e601e8d9b300ee6f3a1
unicred666 <Varchyn207@gmail.com>
2022-07-29 22:28:45 +0300

    edit IbM

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        IbM service = new IbM();
+        float m = 84.5F;
+        float h = (float) Math.pow(1.79, 2);
+        float ibm = service.calculate(m, h);
+        System.out.printf("%.3f", ibm);
+    }
+}
--- src/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        IbM service = new IbM();
-        float m = 84.5F;
-        float h = (float) Math.pow(1.79,2);
-        float ibm = service.calculate(m,h);
-        System.out.printf("%.3f",ibm);
-    }
-}
